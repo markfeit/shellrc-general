a1d75cd5392c7e853a27f750b3f00e9de9c22571
Mark Feit <mfeit@notonthe.net>
2020-01-15 15:32:47 -0500

    Don't export the BASH prompt

--- 90-prompt.sh
+++ 90-prompt.sh
@@ -12,12 +12,12 @@ TTY=${TTY:-$(tty)}
 unset PROMPT_COMMAND
 case "${TERM}" in
     screen|screen.*)
-	export PS1="\e]0;\u@\h:${TTY#/dev/} \w\a\[\e[30;47m\][8^)\[\e[m\]"
+	PS1="\e]0;\u@\h:${TTY#/dev/} \w\a\[\e[30;47m\][8^)\[\e[m\]"
 	;;
     xterm*)
-	export PS1="\e]0;\u@\h:${TTY#/dev/} \w\a\h \[\e[30;47m\][8^)\[\e[m\]"
+	PS1="\e]0;\u@\h:${TTY#/dev/} \w\a\h \[\e[30;47m\][8^)\[\e[m\]"
 	;;
     *)
-	export PS1="\h \[\e[30;47m\][8^)\[\e[m\]"
+	PS1="\h \[\e[30;47m\][8^)\[\e[m\]"
 	;;
 esac
