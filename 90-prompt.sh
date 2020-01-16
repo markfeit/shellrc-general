c9c2caa24b5c007d39a5f136c101092d5200147b
Mark Feit <mfeit@notonthe.net>
2020-01-15 19:51:48 -0500

    Change prompt color according to exit status

--- 90-prompt.sh
+++ 90-prompt.sh
@@ -10,14 +10,20 @@ TTY=${TTY:-$(tty)}
 # Bart Head:   "\[\e[30;47m\][8^)\[\e[m\]"
 
 unset PROMPT_COMMAND
+
+prompt_color()
+{
+    [ $1 -eq 0 ] && printf "30;107" || printf "97;41"
+}
+
 case "${TERM}" in
     screen|screen.*)
-	PS1="\e]0;\u@\h:${TTY#/dev/} \w\a\[\e[30;47m\][8^)\[\e[m\]"
+	PS1='\e]0;\u@\h:${TTY#/dev/} \w\a\[\e[`prompt_color $?`m\][8^)\[\e[m\]'
 	;;
     xterm*)
-	PS1="\e]0;\u@\h:${TTY#/dev/} \w\a\h \[\e[30;47m\][8^)\[\e[m\]"
+	PS1='\e]0;\u@\h:${TTY#/dev/} \w\a\h \[\e[`prompt_color $?`m\][8^)\[\e[m\]'
 	;;
     *)
-	PS1="\h \[\e[30;47m\][8^)\[\e[m\]"
+	PS1='\h \[\e[`prompt_color $?`m\][8^)\[\e[m\]'
 	;;
 esac
