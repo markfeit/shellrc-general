b471ff26a77d48a75a1fc8a3fc4527936398df62
Mark Feit <mfeit@notonthe.net>
2020-01-24 07:56:59 -0500

    Don't count hard status as part of the prompt length.

--- 90-prompt.sh
+++ 90-prompt.sh
@@ -6,7 +6,7 @@
 TTY=${TTY:-$(tty)}
 
 # Text snippets:
-# Hard status: "\e]0;\u@\h:${TTY#/dev/} \w\a"
+# Hard status: "\[\e]0;\u@\h:${TTY#/dev/} \w\a\]"
 # Bart Head:   "\[\e[30;47m\][8^)\[\e[m\]"
 
 unset PROMPT_COMMAND
@@ -18,10 +18,10 @@ prompt_color()
 
 case "${TERM}" in
     screen|screen.*)
-	PS1='\e]0;\u@\h:${TTY#/dev/} \w\a\[\e[`prompt_color $?`m\][8^)\[\e[m\]'
+	PS1='\[\e]0;\u@\h:${TTY#/dev/} \w\a\]\[\e[`prompt_color $?`m\][8^)\[\e[m\]'
 	;;
     xterm*)
-	PS1='\e]0;\u@\h:${TTY#/dev/} \w\a\h \[\e[`prompt_color $?`m\][8^)\[\e[m\]'
+	PS1='\[\e]0;\u@\h:${TTY#/dev/} \w\a\]\h \[\e[`prompt_color $?`m\][8^)\[\e[m\]'
 	;;
     *)
 	PS1='\h \[\e[`prompt_color $?`m\][8^)\[\e[m\]'
