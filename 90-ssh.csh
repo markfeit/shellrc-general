1cab8873e9aa8c4f5e4515f8d6ebfb49f978b029
Mark Feit <mfeit@notonthe.net>
2020-01-28 15:27:49 -0500

    Run keychain if it's present

--- 90-ssh.csh
+++ 90-ssh.csh
@@ -6,3 +6,7 @@
 alias ka "ssh-add -t 7200 -A"
 alias kl "ssh-add -l"
 alias kd "ssh-add -D"
+
+set kctype=`type -t keychain`
+eval `[ -n "$kctype" ] && env SHELL=/bin/csh keychain --eval id_rsa || true`
+unset kctype
--- 90-ssh.sh
+++ 90-ssh.sh
@@ -6,3 +6,8 @@
 alias ka="ssh-add -t 7200 -A"
 alias kl="ssh-add -l"
 alias kd="ssh-add -D"
+
+
+# If keychain is anywhere in the path, use it.
+
+eval `[ -n "$(type -t keychain)" ] && keychain --quiet --eval id_rsa || true`
