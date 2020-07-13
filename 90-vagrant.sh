b37493817bb1efc002e5188ee7e3e1b1dfd07d3e
Mark Feit <mfeit@notonthe.net>
2020-07-13 12:02:41 -0400

    Added vagrant SSH-to-root alias

--- 90-vagrant.sh
+++ 90-vagrant.sh
@@ -5,6 +5,7 @@
 alias vdf="vagrant destroy -f"
 alias vdfu="vagrant destroy -f && vagrant up"
 alias vssh="vagrant ssh"
+alias vsshr='vagrant ssh -c "sudo -i"'
 alias vsshu='vagrant ssh -c "sudo -i -u ${USER}"'
 alias vup="vagrant up"
 alias vupp="vagrant up --provision"
