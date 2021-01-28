7aff0641ef7b28bd0ac8777993e91e80e018eb02
Mark Feit <mfeit@notonthe.net>
2021-01-28 16:41:19 -0500

    Make screen alias smarter about unwritable TTYs

--- 10-screen.sh
+++ 10-screen.sh
@@ -2,8 +2,13 @@
 # Screen Setup
 #
 
-# Screen for full keyboards
-alias sc="screen -e '^\^\' -R -A -d"
+# Screen for full keyboards.  On some systems, if you've su'd, the tty
+# is unwritable, so work around that.
+[ -w $(tty) ] \
+    && alias sc='screen -e '\''^\^\'\'' -R -A -d' \
+    || alias sc="script /dev/null -c 'screen -e '\''^\^\'\'' -R -A -d'" \
+
+
 
 # For mobile keyboards where the backslash is difficult
 alias so="screen -e '^O^O' -R -A -d"
